21e386253778a3f6d0cdfebe28a5142a2c64c70a
Ivo Dolenc <ivodolenc@gmail.com>
2021-08-09 15:37:09 +0200

    refactor: update router scrollBehavior

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -1,1 +1,11 @@
-export default function (to, from, savedPosition) {}
+export default function (to, from, savedPosition) {
+  if (savedPosition) {
+    return savedPosition
+  } else if (to.hash) {
+    return {
+      selector: to.hash,
+      offset: { x: 0, y: 100 }
+    }
+  }
+  return { x: 0, y: 0 }
+}
